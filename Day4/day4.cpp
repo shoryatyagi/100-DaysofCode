e88d6c36044a2009b6c79acf0dfdc77f579a804f
Shorya Tyagi <shoryatyagi29@gmail.com>
2023-04-15 14:28:30 +0530

    odd or even using function

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+
+/*
 // ? Mini calculator using switch case
 
 int main(){
@@ -27,7 +29,29 @@ int main(){
                 cout<< a/b<<endl;
                 break;}
         
+    }
+}
+*/
 
+//! checking even or odd using function
+bool isEven(int a ){
+    if (a&1){
+        return 0;
+    }
+    else{
+        return 1;
+    }
+}
 
+int main(){
+    int num;
+    cin >> num;
+    
+    if (isEven(num)){
+        cout<< "The number is even"<<endl;
+
+    }
+    else{
+        cout<< "The number is odd"<<endl;
     }
 }
