59de6f657655d4801076f2f21e434ac2927ec3c3
Shorya Tyagi <shoryatyagi29@gmail.com>
2023-04-16 16:49:05 +0530

    nCr using function

--- Day4/day4.cpp
+++ Day4/day4.cpp
@@ -33,6 +33,7 @@ int main(){
 }
 */
 
+/*
 //! checking even or odd using function
 bool isEven(int a ){
     if (a&1){
@@ -54,4 +55,40 @@ int main(){
     else{
         cout<< "The number is odd"<<endl;
     }
+}
+*/
+
+//! Finding nCr using function
+
+int nCr(int n , int r){
+
+
+    int num = 1;
+    for (int i = 1; i <=n; i++){
+        num *= i;
+    
+    }
+    int deno = 1;
+    for (int j = 1; j <=r; j++){
+        deno *= j;
+         }
+    for(int k =1 ; k <= n-r; k++){
+        deno *= k;
+    }
+    int ans = num/deno;
+    return ans;
+}
+
+int main(){
+    int n, r;
+    cout<< "Enter the value of n : ";
+    cin >> n ;
+    cout << endl;
+
+    cout<< "Enter the value of r : ";
+    cin >> r;
+    cout << endl;
+
+    int result = nCr(n,r);
+    cout<< "Answer of nCr : " << result << endl;
 }
