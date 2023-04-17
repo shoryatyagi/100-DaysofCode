cab8f59288a49d25851fdf5495116f95ce5f583b
Shorya Tyagi <shoryatyagi29@gmail.com>
2023-04-17 12:57:30 +0530

    linear search algorithm

--- Day5/day5.cpp
+++ Day5/day5.cpp
@@ -34,6 +34,7 @@ int main(){
 }
 */
 
+/*
 //? Max and Min in array
 
 int getMin(int arr[],int n){
@@ -69,7 +70,29 @@ int main(){
 
     cout << "Maximum Value : " << getMax(num,size) << endl;
     cout << "Minimum Value : " << getMin(num,size) << endl;
-
-
 }
+*/
+
+// ? linear search algorithm
+
+bool search(int arr[],int n,int key){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == key)  return 1;
+    }
+    return 0;
+}
+
+int main(){
+    int arr[10] = {0,-2,3,4,5,6,-4,8,-3,10};
+    int key;
+    cout<< "Enter the key you want to search : ";
+    cin>> key;
+    if (search(arr,10,key)){
+        cout << "Key is present !";
+    }
+    else{
+        cout << "Key is not present !";
+    }
+}
+
 
